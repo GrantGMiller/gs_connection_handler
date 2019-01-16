4d837228bf8cecd62fd8e4aa8aec97185ce466d5
gmiller <gmiller@extron.com>
2019-01-16 14:56:36 -0500

    new feature - multiple cdar servers

--- test-files/server_flood.py
+++ test-files/server_flood.py
@@ -6,11 +6,13 @@ Good luck!
 
 from extronlib.interface import EthernetClientInterface
 
-for i in range(300):
+for i in range(400):
     print('i=', i)
-    c = EthernetClientInterface('10.8.27.36', 9000)
+    c = EthernetClientInterface('10.8.27.21', 3888)
     c.Connected = lambda interface, s, i=i: print('event', i, s, interface)
     c.Disconnected = lambda interface, s, i=i: print('event', i, s, interface)
     res = c.Connect(1)
-    # print('res=', res)
-    # c.Disconnect()
+    if res in ['Connected']:
+        c.Send('Sent from {}'.format(i))
+
+    # c.Disconnect() # un/comment this to test conneting then disconnecting quickly
